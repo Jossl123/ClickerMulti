4b77852a781ccb5228f208b34e02f58814e19708
Jossl123 <jossl1.gau@gmail.com291204Jg291204JG>
2022-09-14 22:23:15 +0200

    make Draggable window

--- public/index.js
+++ public/index.js
@@ -9,3 +9,27 @@ function tooglePanel(name) {
         document.getElementById(name).style.visibility = "hidden"
     }
 }
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("window", JSON.stringify({
+        id: ev.target.id,
+        x: ev.clientX,
+        y: ev.clientY
+    }));
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = JSON.parse(ev.dataTransfer.getData("window"));
+    var boundingRec = document.getElementById(data.id).getBoundingClientRect()
+    document.getElementById(data.id).style.left = (ev.clientX - data.x + boundingRec.left) + "px"
+    document.getElementById(data.id).style.top = (ev.clientY - data.y + boundingRec.top) + "px"
+}
+
+window.addEventListener("resize", () => {
+    return
+})
