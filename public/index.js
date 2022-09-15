e7806de6eb14f9bac130f648681efad79866f9d3
Jossl123 <jossl1.gau@gmail.com>
2022-09-15 10:08:25 +0200

    make window more fery Good

--- public/index.js
+++ public/index.js
@@ -10,10 +10,6 @@ function tooglePanel(name) {
     }
 }
 
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
 function drag(ev) {
     ev.dataTransfer.setData("window", JSON.stringify({
         id: ev.target.id,
@@ -28,6 +24,35 @@ function drop(ev) {
     var boundingRec = document.getElementById(data.id).getBoundingClientRect()
     document.getElementById(data.id).style.left = (ev.clientX - data.x + boundingRec.left) + "px"
     document.getElementById(data.id).style.top = (ev.clientY - data.y + boundingRec.top) + "px"
+}
+//////
+var element_dragged = ""
+var offset_dragged = {x: 30, y: 10}
+function element_drag(ev) {
+    element_dragged = ev.target.id
+    drop(ev)
+}
+
+function drag_over(event) {
+    event.preventDefault()
+    return false
+}
+function element_drag_ghost(ev) {
+    var boundingRec = document.getElementById(ev.target.id).getBoundingClientRect()
+    offset_dragged = {x: ev.clientX - boundingRec.left, y: ev.clientY - boundingRec.top}
+    ev.dataTransfer.setDragImage(document.createElement('img'), 0, 0)
+}
+function drop(ev) {
+    var dm = document.getElementById(element_dragged)
+    if (ev.clientX > 0 || ev.clientY > 0) {
+        dm.style.left = ev.clientX - offset_dragged.x + 'px'
+        dm.style.top = ev.clientY - offset_dragged.y + 'px'
+    }
+    ev.preventDefault();
+    // var data = JSON.parse(ev.dataTransfer.getData("window"));
+    // var boundingRec = document.getElementById(data.id).getBoundingClientRect()
+    // document.getElementById(data.id).style.left = (ev.clientX - data.x + boundingRec.left) + "px"
+    // document.getElementById(data.id).style.top = (ev.clientY - data.y + boundingRec.top) + "px"
 }
 
 window.addEventListener("resize", () => {
